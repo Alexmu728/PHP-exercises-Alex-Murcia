1ebbcd4c8c6cf06639b54694c97bf054715c31b5
DW32 TALDEA <ik012982i10@H012982.NET>
2024-09-09 13:27:23 +0200

    execises 1 and 2 from the first block done

---
+++ site.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+
+<html>
+
+	<head>
+
+		<meta charset="utf-8">
+
+		<title></title>
+
+	</head>
+
+	<body>
+
+		<?php
+
+			$floors= 2;
+			$doors= 8;
+
+			echo " $floors floors have $doors doors."
+		?>
+
+	</body>
+
+</html>
