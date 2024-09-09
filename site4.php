5c4f93feb3e85d04408b765ef4e2183c3e6e278a
DW32 TALDEA <ik012982i10@H012982.NET>
2024-09-09 13:56:48 +0200

    Started the fourth exercise

---
+++ site4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+
+<html>
+
+	<head>
+
+		<meta charset="utf-8">
+
+		<title></title>
+
+	</head>
+
+	<body>
+
+		<?php
+
+		
+            $word= "anana"
+            
+
+
+		?>
+
+	</body>
+
+</html>
