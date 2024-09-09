2d52e37ce3f38995a1e98588778840f2df4c65b9
DW32 TALDEA <ik012982i10@H012982.NET>
2024-09-09 13:29:47 +0200

    execises 1 and 2 from the first block done

---
+++ site2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+
+<html>
+
+	<head>
+
+		<meta charset="utf-8">
+
+		<title></title>
+
+	</head>
+
+	<body>
+
+		<?php
+
+			$biggest=0;
+            $num1 = rand(1, 100);
+            $num2 = rand(1, 100);
+            $num3 = rand(1, 100);
+
+            if($num1>$biggest){
+                $biggest=$num1;
+            }
+            if($num2>$biggest){
+                $biggest=$num2;
+            }
+            if($num3>$biggest){
+                $biggest=$num3;
+            }
+
+            echo "$num1<br>$num2<br>$num3<br>";
+            echo "The biggest number is $biggest";
+
+		?>
+
+	</body>
+
+</html>
