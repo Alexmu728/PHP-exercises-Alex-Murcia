0ad51f6906fea6395c6951e38be037c56e30faa4
DW32 TALDEA <ik012982i10@H012982.NET>
2024-09-16 13:14:40 +0200

    Exercise 4 done

---
+++ Block2/site4.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+
+<html>
+
+	<head>
+
+		<meta charset="utf-8">
+
+		<title></title>
+
+	</head>
+
+	<body>
+
+		<?php
+
+			$string="last god spider requiem knight";
+
+            $words=explode(" ", $string);
+
+            $array=[];
+
+            foreach($words as $key){
+                $array[$key]=strlen($key);
+            }
+            var_dump($array);
+
+            
+
+		?>
+
+	</body>
+
+</html>
