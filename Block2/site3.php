f0d0a621b2acf592f47630719204b24d7aeedb2a
DW32 TALDEA <ik012982i10@H012982.NET>
2024-09-16 12:51:36 +0200

    Exercise 3 done

--- Block2/site3.php
+++ Block2/site3.php
@@ -14,22 +14,75 @@
 
 		<?php
 
-			function createTable($num1, $num2){
-                echo "<table border='1'>";
+            $array=[];
+            $count=0;
 
-                for($i=0;$i<$num1;$i++){
-                    echo "<tr>";
-                    for($j=0;$j<$num2;$j++){
-                        echo "<td>table</td>";
-                    }
-                    echo "</tr>";
-                }
+			for($i=0;$i<20;$i++){
+                $array[$i]= rand(1,50);
+                $count++;
+            } 
+
+            foreach($array as $num){
+                echo "$num ||";
             }
 
-            $num1= rand(1,10);
-            $num2= rand(1,10);
+            echo "<br>";
+            echo "<br>";
+            
 
-            createTable($num1, $num2);
+            $biggest=0;
+
+            /*function biggest($num){
+                for($i=0;$i<$num.length;$i++){
+                    if($num[i]>$biggest){
+                        $biggest=$num[i];
+                    }
+                }
+                echo "The biggest number is "+$biggest;
+            }
+
+            function smallest($num){
+                for($i=0;$i<$num.length;$i++){
+                    if($num[i]<$smallest){
+                        $smallest=$num[i];
+                    }
+                }
+                echo "The smallest number is "+$smallest;
+            }*/
+
+            sort($array);
+
+            foreach($array as $num){
+                echo "$num ||";
+                if($num>$biggest){
+                    $biggest=$num;
+                }
+            }
+            
+            echo "<br>";
+            echo "<br>";
+            echo "<br>";
+
+            $smallest=$array[0];
+
+            $sum=array_sum($array);
+
+            $mean= $sum/$count;
+
+            echo "Smallest: <p style='color:blue'>$smallest</p>";
+
+            echo "<br>";
+
+            echo "Biggest: <p style='color:red'>$biggest</p>";
+
+            echo "<br>";
+
+            echo "Sum: $sum";
+
+            echo "<br>";
+
+            echo "Mean: $mean";
+
 
 		?>
 
