936e0a9b7e64a1e86152aa31325db70757fae57e
DW32 TALDEA <ik012982i10@H012982.NET>
2024-09-25 10:12:28 +0200

    Index.php done

---
+++ Top Movies/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Index</title>
+    </head>
+    <body>
+
+    <form action="movies.php">
+
+    <label for="user">Enter your name:</label><br>
+    <input type="text" id="user" name="user"><br><br>
+
+    <input type="submit" value="Submit">
+    </form>
+    </body>
+</html>
