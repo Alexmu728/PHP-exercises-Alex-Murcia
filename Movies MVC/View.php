853a90c0d34cbbd18e22d80dd3bfc920e2d0d888
DW32 TALDEA <ik012982i10@H012982.NET>
2024-10-02 10:39:21 +0200

    Made that the info when adding a film stays once is added

--- Movies MVC/View.php
+++ Movies MVC/View.php
@@ -27,28 +27,29 @@
             <form method="POST" action="Controller.php">
 
                 <label for="name">Name of the film:</label><br>
-                <input type="text" id="name" name="name"><br><br>
+                <input type="text" id="name" name="name" value="<?php echo isset($_POST['name']) ?htmlspecialchars($_POST['name']):'';?>"><br><br>
 
                 <label for="isan">ISAN number:</label><br>
-                <input type="text" id="isan" name="isan"><br><br>
+                <input type="text" id="isan" name="isan" value="<?php echo isset($_POST['isan']) ?htmlspecialchars($_POST['isan']):'';?>"><br><br>
 
                 <label for="year">Movie year:</label><br>
-                <input type="text" id="year" name="year"><br><br>
+                <input type="text" id="year" name="year" value="<?php echo isset($_POST['year']) ?htmlspecialchars($_POST['year']):'';?>"><br><br>
 
                 <label for="punctuation">Punctuation: </label><br>
                 <select id="punctuation" name="punctuation">
-                    <option value='0' >0</option>
-                    <option value='1' >1</option>
-                    <option value='2' >2</option>
-                    <option value='3' >3</option>
-                    <option value='4' >4</option>
-                    <option value='5' >5</option>
+                    <option value='0'<?php echo (isset($_POST['punctuation']) && $_POST['punctuation']== '0')?'selected' :'';?>>0</option>
+                    <option value='1'<?php echo (isset($_POST['punctuation']) && $_POST['punctuation']== '1')?'selected' :'';?>>1</option>
+                    <option value='2'<?php echo (isset($_POST['punctuation']) && $_POST['punctuation']== '2')?'selected' :'';?>>2</option>
+                    <option value='3'<?php echo (isset($_POST['punctuation']) && $_POST['punctuation']== '3')?'selected' :'';?>>3</option>
+                    <option value='4'<?php echo (isset($_POST['punctuation']) && $_POST['punctuation']== '4')?'selected' :'';?>>4</option>
+                    <option value='5'<?php echo (isset($_POST['punctuation']) && $_POST['punctuation']== '5')?'selected' :'';?>>5</option>
                 </select>
                 <br><br>
 
                 <input type="submit" value="Submit">
             </form>
         </div>
+
 
         <h2>Current Movies:</h2>
         <ul>
