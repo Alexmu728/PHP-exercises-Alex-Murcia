df2fcd7f1ef8cdd7f7506494c49eb016da336c29
DW32 TALDEA <ik012982i10@H012982.NET>
2024-10-04 10:27:58 +0200

    modified runner.php

---
+++ Block 3/Runner.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML>
+<html>
+
+	<head>
+
+		<meta charset="utf-8">
+
+		<title></title>
+
+	</head>
+
+	<body>
+
+		<?php
+
+			class Runner{
+                private $name;
+                private $code;
+                private $raceTimes=array();
+
+                public function __construct($name, $code){
+                    $this->name=$name;
+                    $this->code=$code;
+                }
+
+
+                public function getName(){
+                    return $this->name;
+                }
+
+
+                
+                public function getCode(){
+                    return $this->code;
+                }
+               
+                public function getRaceTimes(){
+                    return $this->raceTimes;
+                }
+
+                public function addRace($time){
+                    if(count($this->raceTimes)>=5){
+                        throw new Exception("The runner already has 5 races.");
+                    }
+                    if($time<5){
+                        throw new Exception("There's a race that is less than 5 seconds.");
+                    }
+                    $this->raceTimes[]=$time;
+                }
+
+            }
+
+		?>
+
+	</body>
+
+</html>
+
