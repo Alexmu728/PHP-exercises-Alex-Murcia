977aa4b1bef29e60a246d10ee3af1e87da93e29b
DW32 TALDEA <ik012982i10@H012982.NET>
2024-10-04 09:58:47 +0200

    something

--- Block 3/Competition.php
+++ Block 3/Competition.php
@@ -22,7 +22,33 @@
                 }
 
                 public function addRaceToRunner($code, $time){
-                    addRace($time);
+                    $this->runners[$code]->addRace($time);
+                }
+
+                public function average(){
+                    $average=0;
+                    $i=0;
+
+                    foreach($this->runners as $runner){
+                        $time=$runner->getRaceTimes();
+                        $average=$average+$time[0];
+                        $i++;
+                    }
+                    return ($average/$i);
+                }
+
+                public function quickest(){
+                    $quick=9999999;
+                    $quickest="";
+                    foreach($this->runners as $runner){
+                        foreach($runner->getRaceTimes() as $time){
+                            if($time<$quick){
+                                $quick=$time;
+                                $quickest=$runner->getName();
+                            }
+                        }
+                    }
+                    return "The quickest runner is $quickest with $quick time";
                 }
             }
 
