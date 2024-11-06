e8f446071173f59b182182252ad3bf1df16ceae1
Alexmu728 <amurciaqu23@ikzubirimanteo.com>
2024-11-06 07:37:49 +0000

    Task model configured

--- AlexMurciaCityTaskList/app/Models/Task.php
+++ AlexMurciaCityTaskList/app/Models/Task.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     //
+    public function citizen(){
+        return $this->belongsTo(Citizen::class);
+    }
+
+    public function subjects(){
+        return $this->belongsToMany(Subject::class, "subject_task");
+    }
 }
