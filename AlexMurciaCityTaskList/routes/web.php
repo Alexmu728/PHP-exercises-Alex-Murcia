98759633ef0b6f13a8a3fd982372b028d781e89a
Alexmu728 <amurciaqu23@ikzubirimanteo.com>
2024-11-06 08:56:02 +0000

    Routes added

--- AlexMurciaCityTaskList/routes/web.php
+++ AlexMurciaCityTaskList/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CitizenController;
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,4 +16,5 @@ Route::resource("tasks", TaskController::class);
 Route::resource("subjects", SubjectController::class);
 
 Route::get("/task/recent", [TaskController::class, "recent"]);
-Route::get("/tasks/filter", [TaskController::class, "filter"]);
+Route::get("/tasks/filter", [TaskController::class, "filter"])->name("tasks.filter");
+Route::get("/tasjs/search", [TaskController::class, "search"])->name("tasks.search");
