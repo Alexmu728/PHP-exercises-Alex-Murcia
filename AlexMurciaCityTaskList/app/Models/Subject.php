259ae16d6bbd0a74905184c70cbd2bf578be5be6
Alexmu728 <amurciaqu23@ikzubirimanteo.com>
2024-11-06 07:38:49 +0000

    Subject model configured

--- AlexMurciaCityTaskList/app/Models/Subject.php
+++ AlexMurciaCityTaskList/app/Models/Subject.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model
 {
     //
+    public function tasks(){
+        return $this->belongsToMany(Task::class, "subject_task");
+    }
 }
