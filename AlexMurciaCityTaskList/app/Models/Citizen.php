4cab978f5daba9646e0981ca6573d345c0df37f2
Alexmu728 <amurciaqu23@ikzubirimanteo.com>
2024-11-06 07:35:41 +0000

    Citizen model configured

--- AlexMurciaCityTaskList/app/Models/Citizen.php
+++ AlexMurciaCityTaskList/app/Models/Citizen.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Citizen extends Model
 {
     //
+    public function address(){
+        return $this->hasOne(Address::class);
+    }
+    public function tasks(){
+        return $this->hasMany(Task::class);
+    }
 }
